ec2a20cbda4075c7d1e015a2c0d7395c8c118b04
Isaac Idibia <idibiaisaac3@gmail.com>
2022-11-25 00:19:32 +0100

    feat(FE): added base styling to tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      background: "#F3F5FF",
+      primary: "#0B0A33",
+      secondary: "#B2255F",
+      countDown: "#4D4C59",
+      placeholders: "#888888",
+      counterBg: "background: rgba(178, 37, 95, 0.8)",
+      counterBoxShadow: "box-shadow: 0px 4px 8px rgba(178, 37, 95, 0.15)",
+    },
     extend: {},
   },
   plugins: [],
