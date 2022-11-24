1370618fd0a9daca60e072de00415664aee94697
davidadediji <davidadediji@gmail.com>
2022-11-25 00:33:08 +0100

    feat:endpoint for creating countdown

--- server/src/controllers/countdown.controllers.js
+++ server/src/controllers/countdown.controllers.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const Countdown = require('../models/countdown.model');
 
+const createCountdown = async (req, res) => {
+  try {
+      const countdown = await Countdown.create(req.body)
+      res.status(201).json({ success: true, task })
+  } catch (error) {
+      res.status(500).json({ msg: error.message })
+  }
+
+}
+
 const getCountDown = async (req, res) => {
   const events = await Countdown.find();
   try {
