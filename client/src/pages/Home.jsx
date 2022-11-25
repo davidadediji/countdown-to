11442e2ee3e1895bc67c7d12c4eccfb51f34dd2f
Isaac Idibia <idibiaisaac3@gmail.com>
2022-11-25 01:09:44 +0100

    feat(FE): folder structure, home and header

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main className='max-w-screen-xl m-auto text-center'>
+        <h1>Home Page</h1>
+    </main>
+  )
+}
+
+export default Home;
