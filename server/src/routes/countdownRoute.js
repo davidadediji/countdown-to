59f2c69371625e64d6cc906c326d29dfb2cc5ac3
davidadediji <davidadediji@gmail.com>
2022-11-25 12:27:30 +0100

    chore:create swagger documentation for endpoints post, put

--- server/src/routes/countdownRoute.js
+++ server/src/routes/countdownRoute.js
@@ -4,16 +4,56 @@ const {
   createCountdown,
   updateCountDown,
 } = require('../controllers/countdown.controllers');
+/**
+ *  @swagger
+ *    components:
+ *      schemas:
+ *        Countdown:
+ *          type: object
+ *          required:
+ *            - email
+ *            - countdownTitle
+ *            - countdownDate
+ *          properties:
+ *            id:
+ *              type: integer
+ *              description: The auto-generated objectid of the countdown event.
+ *            email:
+ *              type: string
+ *              description: The email of the user
+ *            countdownTitle:
+ *              type: string
+ *              description: Title of the countdown event
+ *            countdownDescription:
+ *              type: string
+ *              description: Description of the countdown event
+ *            countdownDate:
+ *              type: string
+ *              format: date
+ *              description: Date for the countdown
+ *          example:
+ *             email: sam@gmail.com
+ *             countdownTitle: Christmas Day
+ *             countdownDate: 12-25-2022
+*/
 
+/**
+ * @swagger
+ * tags:
+ *     - name: Countdown Events
+ *       description: countdown apis
+ */
 
 /**
  * @swagger
  * /api/v1/countdown:
  *   get:
  *     summary: Retrieve a list of countdown events.
+ *     tags:
+ *        - Countdown Events
  *     responses:
  *       200:
- *         description: A list of users.
+ *         description: A list of events.
  *         content:
  *           application/json:
  *             schema:
@@ -22,10 +62,51 @@ const {
  */
 router.get('/', getCountDown);
 
-
+/**
+ * @swagger
+ * /api/v1/countdown:
+ *     post:
+ *         summary: user creates a countdown event
+ *         tags:
+ *             - Countdown Events
+ *         requestBody:
+ *             description: a json with all fields
+ *             required: true
+ *             content:
+ *                 application/json:
+ *                     schema:
+ *                          $ref: '#/components/schemas/Countdown'
+ *         responses:
+ *             '201':
+ *                description: Created count down event successfully
+ *                content:
+ *                    application/json:
+ *                        schema:
+ *                            type: object
+ */
 router.post('/', createCountdown);
 
-
+/** 
+ * @swagger
+ * /api/v1/countdown/{id}:
+ *      put:
+ *         summary: update count down event
+ *         tags:
+ *             - Countdown Events
+ *         parameters:
+ *             - in: path
+ *               name: id
+ *               schema:
+ *                    type: string
+ *                    required: true
+ *         requestBody:
+ *             description: a json with all fields
+ *             required: true
+ *             content:
+ *                 application/json:
+ *                     schema:
+ *                          $ref: '#/components/schemas/Countdown'
+*/
 router.put('/updateCountDown', updateCountDown);
 
 module.exports = router;
