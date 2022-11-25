164448de1ad738cc39f639c9170c979d0e5716cd
Isaac Idibia <idibiaisaac3@gmail.com>
2022-11-25 02:12:21 +0100

    feat(FE): full header

--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Button from '../ui/Button';
+import { Link } from "react-router-dom";
+import { ReactComponent as ArrowRight } from "../assets/ArrowRight.svg";
+import Logo from "../assets/Logo.png";
 
 const Header = () => {
+    const navLinkElements = [
+        { name: "FAQ", url: "#faq" },
+        { name: "Features", url: "#features" },
+        { name: "Use Cases", url: "#use-cases" },
+    ];
+
+    const navLinks = navLinkElements.map((item) => (
+        <li key={item.name}>
+            <Link to={item.url}>{item.name}</Link>
+        </li>
+    ))
+
   return (
-    <header className='max-w-screen-xl m-auto'>Header</header>
+    <header className='max-w-screen-xl m-auto flex items-center justify-between p-5'>
+        <div>
+            <img src={Logo} alt="County" className='w-[199.31px] h-[63.31px]' />
+        </div>
+        <ul className='flex gap-10'>
+            {navLinks}
+        </ul>
+        <div>
+            <Button className="bg-secondary flex gap-[10px] text-white py-[17px] px-8 rounded-[64px]">Get started <ArrowRight /></Button>
+        </div>
+    </header>
   )
 }
 
